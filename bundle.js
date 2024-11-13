cff5926288122e74ea06576920475efd1aed2f8a
Rohanwww <himanshuroh54@gmail.com>
2024-11-13 17:53:17 +0530

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,14 @@
+< !doctype html > < html lang = "en" > < head > < meta charset = "utf-8" / > < link rel = "icon"
+href = "/favicon.ico" / > < title > Online Betting Sports & Live Casino Games in India | 1 xbook < /title><meta name="viewport" content="width=device-width,user-scalable=no"/ > < meta name = "theme-color"
+content = "#000000" / > < meta name = "description"
+content = "1xbook introduces online betting sports, Cricket ID, Fun Gaming, exchange games & online casino games real money for a perfect betting games experience." / > < link rel = "apple-touch-icon"
+href = "/favicon.ico" / > < link rel = "stylesheet"
+href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" / > < link rel = "manifest"
+href = "/manifest.json" / > < script >
+  const timestamp = "1729158238622";
+let script = document.createElement("script");
+script.src = "bundle.js?" + timestamp, document.head.appendChild(script) < /script><script>function sprtRdrCommonFunction(n,o){}</script > < script defer = "defer"
+src = "/static/js/main.60c7e575.js" > < /script><link href="/static / css / main .8 dcd7f7f.css " rel="
+stylesheet "></head><body class="
+radheexch "><div id="
+root "></div></body></html>
---
+++ script.js
@@ -0,0 +1,5 @@
+
+    const Url = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=b07e8201e50bf8ac89f416858695a13e&regions=us&markets=h2h,spreads&oddsFormat=american";
+
+    const get
+
